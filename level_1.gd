05844f25484fb400d71365a43e20ac27a0cd2b9f
Capmell <jamelcollege12@gmail.com>
2026-05-03 20:22:42 -0700

    made mostly level 1

--- level_1.gd
+++ level_1.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	
 		if event.is_action_pressed("ReturnToMainMenu"):
-			print("yes")
+			get_tree().change_scene_to_file("res://main_menu.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
